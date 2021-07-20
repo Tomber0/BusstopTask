73784cb9eee62dedcf35c3fcffdbf74bb2634c2c
Tomber0 <aleseyzaicev@gmail.com>
2021-07-21 01:41:14 +0300

    Added station and route to IBus

--- BusstopTask/Bus/IBus.cs
+++ BusstopTask/Bus/IBus.cs
@@ -1,7 +1,15 @@
-﻿namespace BusstopTask.Bus
-{
-    class IBus
-    {
+﻿using System.Collections.Generic;
 
+namespace BusstopTask.Bus
+{
+    interface IBus
+    {
+        public void Run();
+
+
+
+        //public Station Station{get;set;}
+
+        //public Route Route { get; set; }
     }
 }
