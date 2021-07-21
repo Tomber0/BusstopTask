294a8d8effc4f23b809c9b6b2f4a3d31722e6756
Tomber0 <aleseyzaicev@gmail.com>
2021-07-21 20:19:54 +0300

    added filler to IstationRouting

--- BusstopTask/Bus/IStationRouting.cs
+++ BusstopTask/Bus/IStationRouting.cs
@@ -3,6 +3,12 @@ namespace BusstopTask.Bus
 {
     interface IStationRouting
     {
-        
+        //IRoute Route {get; set;}
+
+        //IStation GetNextStation(route)
+
+        //IStation CurrentStation
+
+        //void MoveToNextStation
     }
 }
